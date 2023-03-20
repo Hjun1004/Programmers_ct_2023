179c36a915da674b6b40eb0836e36881460ff220
Hjun1004 <hjun1004@naver.com>
2023-03-20 17:52:33 +0900

    p120839 풀이

---
+++ src/main/java/com/ll/level0/p120839/Main.java
@@ -0,0 +1,18 @@
+package com.ll.level0.p120839;
+
+import java.util.Arrays;
+
+public class Main {
+}
+
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < rsp.length() ; i++){
+            if(rsp.charAt(i) == '2') sb.append("0");
+            else if(rsp.charAt(i) == '0') sb.append("5");
+            else if(rsp.charAt(i) == '5') sb.append("2");
+        }
+        return sb.toString();
+    }
+}
