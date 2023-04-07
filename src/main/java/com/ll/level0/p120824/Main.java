9cf5d455c77c0f9725817b25b49ffed2ab8bacf8
Hjun1004 <hjun1004@naver.com>
2023-04-07 17:39:10 +0900

    p 120824 풀이

---
+++ src/main/java/com/ll/level0/p120824/Main.java
@@ -0,0 +1,15 @@
+package com.ll.level0.p120824;
+
+public class Main {
+}
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0,0};
+        for(int a : num_list){
+            if(a%2 == 0) answer[0]++;
+            else answer[1]++;
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level0/p120824/Tests.java
@@ -0,0 +1,22 @@
+package com.ll.level0.p120824;
+
+
+import com.ll.level0.p120824.Solution;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Tests {
+    @Test
+    @DisplayName("[1, 2, 3, 4, 5] -> [2, 3]")
+    void t1(){
+        assertThat(new Solution().solution(new int[]{1, 2, 3, 4, 5})).isEqualTo(new int[]{2, 3});
+    }
+
+    @Test
+    @DisplayName("[1, 3, 5, 7] -> [0, 4]")
+    void t2(){
+        assertThat(new Solution().solution(new int[]{1, 3, 5, 7})).isEqualTo(new int[]{0, 4});
+    }
+}
